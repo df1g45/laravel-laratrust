7e6996dbf3e84e460869d4f36b0056d9d2a7ebc3
df1g45 <daffa1g45@gmail.com>
2024-09-17 14:39:28 +0700

    update

--- database/migrations/2024_09_15_132502_create_articles_table.php
+++ database/migrations/2024_09_15_132502_create_articles_table.php
@@ -18,7 +18,7 @@ class CreateArticlesTable extends Migration
             $table->unsignedBigInteger('user_id');
             $table->string('title');
             $table->string('body');
-            $table->boolean('published')->dafault(false);
+            $table->boolean('published')->default(false);
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
